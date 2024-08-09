10c7ce9dd2b04407747e7e62705accc92be7ce3b
NavyaPalle2001 <navya.palle@astraaim.co.uk>
2024-08-09 13:14:44 +0530

    switch task completed

---
+++ Switch.js
@@ -0,0 +1,29 @@
+let day_number = 7;
+let day;
+
+switch(day_number){
+    case 1:
+        day='Monday'
+        break;
+    case 2:
+        day='Tuesday'
+        break;
+    case 3:
+        day='Wednesday'
+        break;
+    case 4:
+        day='Thursday'  
+    case 5:
+        day='Friday'
+        break;
+    case 6:
+        day='Saturday'
+        break;
+    case 7:
+        day='Sunday'
+        break;
+    default:
+        day='Invalid Input'
+}
+
+console.log(day)
